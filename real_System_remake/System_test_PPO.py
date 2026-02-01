64c98437d8179d3ba3a092219821a03d2d17b27b
wx759 <inxwen@outlook.com>
2026-02-01 15:31:49 +0800

    小修改

--- real_System_remake/System_test_PPO.py
+++ real_System_remake/System_test_PPO.py
@@ -279,7 +279,7 @@ class System:
 
     def run(self, seed=None):
         config = Config_PPO(scope='', state_dim=0, action_dim=0, hidden_dim=0)
-        wandb.init(project="CL_learn", workspace="wx829", config={
+        wandb.init(project="paper_draw", workspace="wx829",name=f"PPO_seed{seed}_limday{lim_day}", config={
             "random_seed": seed,
             "is_rms_state": config.is_rms_state,
             "is_rms_reward": config.is_rms_reward,
@@ -553,7 +553,7 @@ class System:
 
 if __name__ == '__main__':
     # for i in range(3):
-    seeds_to_run = [936, 981, 891, 125, 777, 888, 999, 123]
+    seeds_to_run = [378, 652, 894, 17, 512, 739, 83, 291, 956, 105, 421, 668, 33, 777, 184]
     for seed in seeds_to_run:
         print(f"=== 启动 seed={seed} 的实验 ===")
         system = System()
